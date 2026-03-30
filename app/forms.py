4ecfd583e0fd4bc64a924346ea0eba929c8ddada
Sammitings <Sammitings2@gmail.com>
2026-03-30 08:56:13 +0300

    addnew

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,43 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name','brand', 'price', 'specifications', 'category','image']
+
+
+
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Enter password"
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Confirm password"
+    }))
+
+    class Meta:
+        model = User
+        fields = ["email"]
+
+        widgets = {
+            "email": forms.EmailInput(attrs={
+                "placeholder": "Enter your email"
+            })
+        }
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email").lower()
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get("password1")
+        p2 = cleaned_data.get("password2")
+
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError("Passwords do not match.")
+
+        return cleaned_data
