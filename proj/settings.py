abb00d2181b17df7052e59ea0b23a6290cbb5d56
Sammitings <Sammitings2@gmail.com>
2026-04-06 22:38:08 +0300

    release

--- proj/settings.py
+++ proj/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = [
     '.onrender.com',
     'localhost',
     '127.0.0.1',
+    '187.124.210.223'
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -97,13 +98,22 @@ SITE_ID = 1
 # DATABASE
 # ---------------------------------------------------------------------------
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gadgetsoko_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin@123',
+        'HOST': '187.124.210.223',
+        'PORT': '5432',
     }
 }
-
 # To switch to PostgreSQL in production, set DATABASE_URL in .env and uncomment:
 # import dj_database_url
 # DATABASES = {'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)}
